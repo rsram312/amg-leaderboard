23ec2f440b4cd9e258871c6fc20e97f97942fdbd
Sriram <rsriram.rr@gmail.com>
2025-03-26 23:01:57 -0700

    Updates

--- src/vue.config.js
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from '@vue/cli-service'
-
-export default defineConfig({
-  // Set public path correctly for GitHub Pages
-  publicPath: process.env.NODE_ENV === 'production' ? '/amg-leaderboard/' : '/'
-})
